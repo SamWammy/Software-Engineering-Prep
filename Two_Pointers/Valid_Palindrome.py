b5dbb5206f0363c4c749b298c07cf45399003c32
SamWammy <sainfo740@gmail.com>
2025-07-04 14:56:03 -0400

    added optimal solution to two sum II

--- Two_Pointers/Valid_Palindrome.py
+++ Two_Pointers/Valid_Palindrome.py
@@ -14,3 +14,26 @@ class Solution:
         #returns true if palindrome, false if not 
 
         # THERE EXISTS A MORE OPTIMAL SOLUTION, this one is just linear space optimal is constant
+
+
+        ########################################### MORE OPTIMAL APPROACH BELOW O(1) Space
+        #two pointer approach 
+
+        class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # since this is sorted we can take a two pointer approach 
+        # this makes this O(1) space which is optimal 
+
+        left= 0 
+        right= len(numbers) - 1 
+
+        while left < right : 
+            amount= numbers[left] + numbers[right]
+
+            if amount < target: 
+                left +=1
+            elif amount > target:
+                right -=1
+            else: 
+                return [left+1, right +1]
+
