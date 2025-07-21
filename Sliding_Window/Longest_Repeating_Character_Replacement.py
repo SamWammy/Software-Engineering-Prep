31942ac960db8f5fb8aa8f9194a1162358bfbb23
SamWammy <sainfo740@gmail.com>
2025-07-20 21:22:47 -0400

    longest repeating substring

---
+++ Sliding_Window/Longest_Repeating_Character_Replacement.py
@@ -0,0 +1,15 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        tracker ={}
+        res =0 
+        left =0 
+
+        for r in range(len(s)): 
+            tracker[s[r]]= tracker.get(s[r],0)+1
+
+            while (r-left+1) - max(tracker.values())> k: 
+                tracker[s[left]]-=1
+                left+=1
+            res= max(res,r-left+1)
+        return res
+
