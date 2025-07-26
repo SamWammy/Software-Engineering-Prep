98a67c3e8c21488881813d6d8254c26d63e0c31d
SamWammy <sainfo740@gmail.com>
2025-07-26 14:49:06 -0400

    added optimization for max sliding window

--- Sliding_Window/Sliding_Window_Maximum.py
+++ Sliding_Window/Sliding_Window_Maximum.py
@@ -13,3 +13,21 @@ class Solution:
         return final
 
 
+#################################################################
+
+#implementing a deque cuts our time complexity by alot 
+# the above varient uses pop(0) which is O(n) but the below varient uses deque.popleft() which is O(1)
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        left =0 
+        final=[]
+        q= deque()
+        for right in range(len(nums)): 
+            q.append(nums[right])
+
+            if (right-left+1) == k : 
+                final.append(max(q))
+                left +=1 
+                q.popleft()
+        return final
