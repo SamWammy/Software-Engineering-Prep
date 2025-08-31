0529eb020dc5ceda03ac383fd244f56b231dd5a8
SamWammy <sainfo740@gmail.com>
2025-08-31 09:59:21 -0400

    added optimal solution to koko eating bananas

--- Binary_Search/KoKo_eating_bananas.py
+++ Binary_Search/KoKo_eating_bananas.py
@@ -16,3 +16,27 @@ class Solution:
                     return output
             output +=1
 
+#############################################################
+#optimal O(nlog(n)) time solution using binary search 
+
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int: 
+        left,right=1, max(piles)
+        rate=right
+        
+        while left <= right : 
+            middle = (left +right)//2
+            print(middle)
+            hours=0
+            for bananas in piles: 
+                #compute total number of hours
+                hours += math.ceil(bananas/middle)
+            if hours > h: 
+                    #if it took too long we need to find a faster rate
+                left = middle +1
+            else: 
+                    # if it was fast enough we can try to find a faster result
+                rate= middle
+                right = middle -1
+        return rate
