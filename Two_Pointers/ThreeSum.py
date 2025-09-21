cc20a90b95782d0cb4eae79895aac0e46f540c5f
SamWammy <sainfo740@gmail.com>
2025-09-21 15:48:14 -0400

    started again

--- Two_Pointers/ThreeSum.py
+++ Two_Pointers/ThreeSum.py
@@ -1,27 +1,30 @@
+
 class Solution:
+            #[-3,-3,1,2,3,4]
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        #[-3,-3,1,2,3,4]
         res=[]
         nums.sort()
 
-        for i, n in enumerate(nums): 
-            if i >0 and n == nums[i-1]:
+        for i, val in enumerate(nums): 
+            if i > 0 and val == nums[i-1]: 
                 continue 
-            left=i+1
-            right = len(nums)-1
-            
+            left =i+1
+            right =len(nums)-1 
+
             while left< right: 
-                amount= n +nums[left] +nums[right]
-                if amount >0 :
-                    right-=1
-                elif amount <0:
+                total = val +nums[left]+nums[right]
+
+                if total >0: 
+                    right -=1
+                elif total < 0: 
+                    left +=1
+                else:
+                    res.append([val,nums[left],nums[right]])
                     left+=1
-                else: 
-                    res.append([n,nums[left],nums[right]])
-                    left+=1
-                    while nums[left] == nums[left-1] and left< right: 
+                    while nums[left]== nums[left-1] and left < right: 
                         left +=1
-        return res
+        return res 
+
 
         
         # this solution takes a list sorts it, finds the first a (three sum asks for a+b+c=0) and does two sum on the rest of the list to find b and c 
