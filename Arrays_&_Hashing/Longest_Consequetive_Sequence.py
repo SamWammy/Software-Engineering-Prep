1714edd81a8abee21dc76ee7a9f24969c2ebb50b
SamWammy <sainfo740@gmail.com>
2025-12-07 12:22:46 -0500

    redo old problems with other solutions

--- Arrays_&_Hashing/Longest_Consequetive_Sequence.py
+++ Arrays_&_Hashing/Longest_Consequetive_Sequence.py
@@ -13,5 +13,23 @@ class Solution:
                 longest=max(length,longest)
         return longest
 
+#################################################################################
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numset = set(nums)
+        # this gives us O(1) lookup and duplicates shouldnt be counted 
 
-        
+        #we want to find the start of the sequence, and count up from there 
+        longest=0
+
+        for value in nums: 
+            # this checks if we are in the start of the sequence 
+            if value -1 not in numset: 
+                length =1
+                # count the value, start of sequence, count for every value + 1 after
+                while value + 1 in numset: 
+                    length +=1 
+                    value+=1
+                longest= max(longest,length)
+        return longest
+
