153e5b93ac3ce3c2c54481b141b88febe602a42b
SamWammy <sainfo740@gmail.com>
2026-01-29 13:21:31 -0500

    finalized amex prep i used

---
+++ AMEX_Prep/Valid_Palindrome.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        string=""
+        for c in s : 
+            if c.isalnum():
+                string +=c.lower() # make this into lower case 
+        return string == string[::-1]
+
+        #Approach, create a new string with only the alpha numeric chars
+        # (only a-z 1-9) add each character to this string as lower case since we dont care about case sesnitivity 
+        # compare the string with all lower case and only alpha numeric chars 
+        # to its reverse form 
+        #returns true if palindrome, false if not 
+
+        # THERE EXISTS A MORE OPTIMAL SOLUTION, this one is just linear space optimal is constant
+
+
+        
